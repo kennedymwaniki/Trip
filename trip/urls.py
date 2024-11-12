ef4e2893bd186872e27a10a7877b76231ca15c48
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-11-12 12:26:52 +0300

    trip details

--- trip/urls.py
+++ trip/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomeView, trips_list
+from .views import HomeView, TripCreateView, trips_list, TripDetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('dashboard/', trips_list, name='trip-list'),
+    path('dashboard/trip/create', TripCreateView.as_view(), name='trip-create'),
+    path('dashboard/trip/<int:pk>', TripDetailView.as_view(), name='trip-detail'),
 ]
--- trip/views.py
+++ trip/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, DetailView
 # Create your views here.
 from .models import Trip, Note
 
@@ -9,8 +10,34 @@ class HomeView(TemplateView):
 
 
 def trips_list(request):
-    trips = Trip.objects.all()
+    trips = Trip.objects.filter(owner=request.user)
     context = {
         'trips': trips
     }
     return render(request, 'trip/trip_list.html', context)
+
+
+class TripCreateView(CreateView):
+    model = Trip
+    success_url = reverse_lazy('trip-list')
+    fields = ['city', 'country', 'start_date', 'end_date']
+    # expected template is the model name(model_form.html)
+
+    # the form instance is passed in from the view
+    # this will get triggered whenever the form is submitted
+
+    def form_valid(self, form):
+        # owner == logged in owner
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class TripDetailView(DetailView):
+    model = Trip
+    # show notes related to a trip by overriding
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        trip = context["object"]
+        notes = trip.notes.all()
+        context['notes'] = notes
